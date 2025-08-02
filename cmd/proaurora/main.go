c3850e8ec9c30a32c71562e799a0113b7f84fa98
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 16:18:11 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/proaurora/main.go
@@ -0,0 +1,23 @@
+// cmd/proaurora/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"proaurora/internal/proaurora"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := proaurora.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
